f27505eb13b3f45d8e7cd52cad01b25ac925db46
Pranesh <praneshmadhan646@gmail.com>
2025-09-29 12:16:05 +0530

    fixed port issue

--- main.go
+++ main.go
@@ -188,7 +188,7 @@ func main() {
 
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "5000"
+		port = "10000"
 	}
 	log.Println("Running on :" + port)
 	log.Fatal(app.Listen("0.0.0.0:" + port))
